cdbe1678d272554d312781d0c1aaedcc6587c07f
Ali Kurtulush <alikurtulus-92@hotmail.com>
2019-05-09 16:55:19 +0100

    Added components login register and lib file Flash,Auth

---
+++ src/components/auth/Login.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import axios from 'axios'
+
+import Auth from '../../lib/Auth'
+import Flash from '../../lib/Flash'
+
+class Login extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      data: {},
+      error: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    // merge data on state with new data from the form
+    const data = { ...this.state.data, [e.target.name]: e.target.value }
+    // set the data back on state
+    this.setState({ data }) // equivalent to { data: data }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    axios.post('/api/login', this.state.data)
+      .then(res => {
+        Auth.setToken(res.data.token)
+        Flash.setMessage('success', res.data.message)
+        this.props.history.push('/characters')
+      })
+      .catch(() => this.setState({ error: 'Invalid credentials' }))
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-half-desktop is-two-thirds-tablet">
+              <form onSubmit={this.handleSubmit}>
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      name="email"
+                      placeholder="eg: jack@hotmail.com"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Password</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      name="password"
+                      type="password"
+                      placeholder="eg: ••••••••"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+
+                  {this.state.error && <div className="help is-danger">{this.state.error}</div>}
+                </div>
+
+                <button className="button is-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+
+  }
+}
+export default Login
