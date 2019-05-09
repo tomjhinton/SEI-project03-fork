8cbaaec4deb2de3f8ab6638743ec40c69ae00d1e
Ali Kurtulush <alikurtulus-92@hotmail.com>
2019-05-09 17:23:00 +0100

    Added Flash messages

--- src/app.js
+++ src/app.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import 'bulma'
 
-import EventsShow from './components/events/Show'
+import Login from '../components/auth/Login'
+import Register from '../components/auth/Register'
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route path="events/:id" component={EventsShow} />
-        </div>
-      </Router>
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+
+          </Switch>
+        </Router>
+      </main>
     )
   }
 }
