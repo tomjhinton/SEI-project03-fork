40bfe399266aea652607c00171b987d245d4e0cd
Ali Kurtulush <alikurtulus-92@hotmail.com>
2019-05-10 12:05:49 +0100

    Added navbar

--- src/app.js
+++ src/app.js
@@ -4,13 +4,16 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import 'bulma'
 
 import Login from './components/auth/Login'
+import Navbar from './components/common/Navbar'
 import Register from './components/auth/Register'
 
 class App extends React.Component {
   render() {
     return (
       <main>
+
         <Router>
+          <Navbar />
           <Switch>
             <Route path="/register" component={Register} />
             <Route path="/login" component={Login} />
