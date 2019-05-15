de77c446d3807369b66beb8a9e4cf21b03aabc30
Stuart Mees <stuartmees@gmail.com>
2019-05-15 14:36:52 +0100

    conditional rendering on navbar complete

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -42,8 +42,9 @@ class Navbar extends React.Component{
         <div className={`navbar-menu${this.state.active ? ' is-active' : ''}`}>
           <div className="navbar-start">
 
-            <Link to="/events" className={`navbar-item ${this.state.active ? ' is-active' : ''} `}><strong>Events</strong></Link>
-            <Link to="/venues" className={`navbar-item ${this.state.active ? ' is-active' : ''} `}><strong>Venues</strong></Link>
+            {((this.props.location.search!=='' && this.props.location.pathname==='/events')||(this.props.location.pathname!=='/events')) && <Link to="/events" className={`navbar-item ${this.state.active ? ' is-active' : ''} `}><strong>Events</strong></Link>}
+
+            {this.props.location.pathname!=='/venues' && <Link to="/venues" className={`navbar-item ${this.state.active ? ' is-active' : ''} `}><strong>Venues</strong></Link>}
             {Auth.isAuthenticated() && <Link to="/events/new" className={`navbar-item ${this.state.active ? ' is-active' : ''} `}><strong>Add an Event!</strong></Link>}
 
           </div>
