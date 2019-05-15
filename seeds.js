eb456a18a1a5c0fec0150233373aca762439a187
Tom Hinton <tomjhinton@gmail.com>
2019-05-15 09:41:47 +0100

    added seeds

--- seeds.js
+++ seeds.js
@@ -121,6 +121,152 @@ mongoose.connect(dbUri,(err,db) =>{
             }]
 
 
+        },
+        {
+
+          venue: 'Phonox',
+          postcode: 'SW9 7AY',
+          skId: '3377469',
+          name: 'Dj Boring',
+          image: 'https://www.residentadvisor.net/images/events/flyer/2019/6/uk-0615-1253355-front.jpg',
+          date: '2019-06-15T23:00:00.000Z',
+          start: '21:30',
+          finish: '04:00',
+          price: 20,
+          description: 'A close friend of Phonox heads up every Saturday in June, for a not so boring month of parties to dance at alongside your favourite pals.\n Boring\'s rise to the pinnacle of dance music has been through sheer dedication to his craft. Those loose house sounds and vocal samples in his own productions mixed with classics and forward-facing tracks make his sets a complete journey.\n Phonox: Any surprises to come for the residency?\nBoring: You’ll just have to come and find out.\n Phonox: Will Winona Ryder make an appearance?\nBoring: Still waiting on her RSVP.\nPhonox: What are you most looking forward to with the residency?\nBoring: I’ve had some amazing nights at Phonox. I’m looking forward to creating more of those nights and having a big boogie hopefully with some familiar faces. \nDoors for the bar open from 9:30pm, DJ starts at 10pm.',
+          minimumAge: 18,
+          modifiedBy: user,
+          createdBy: user,
+          artist: [
+            {
+              label: 'Dj Boring',
+              value: 'Dj Boring'
+            }
+          ]
+
+
+        },
+        {
+
+          venue: 'Mirth, Marvel & Maud',
+          postcode: 'E17 4QH',
+          skId: '4057204',
+          name: 'Electronic 17: A Guy Called Gerald',
+          image: 'https://www.residentadvisor.net/images/events/flyer/2019/6/uk-0621-1244281-front.jpg',
+          date: '2019-06-21T23:00:00.000Z',
+          start: '20:00',
+          finish: '02:00',
+          price: 12,
+          description: 'An iconic name in dance music, A Guy Called Gerald stands out for consistent innovation, excellence and refusal to compromise. A Guy Called Gerald kick-started Europe’s acid house movement with his ’88 classic ‘Voodoo Ray’ and `Pacific State` (as a founding member of 808 State) and went on to lay down the blueprint for jungle/drum n bass. Eleven albums and 30 years of independence later, he continues to push the boundaries of electronic dance music touring worldwide bringing his "true school" flavour to a world overloaded with pop pap.\nAlthough his remixes are relatively enviable including the likes of David Bowie, Cabaret Voltaire, Black Uhuru, Finley Quaye, Lamb, Tricky and The Stone Roses, it is Gerald’s own productions and refusal to plough anyone’s furrow but his own which has marked him out. A Guy Called Gerald is responsible for the birth of British dance music as you know it today and continues to explore what is possible both in the studio and in the club with his "Live in Session" performances. “',
+          minimumAge: 18,
+          modifiedBy: user,
+          createdBy: user,
+          artist: [
+            {
+              label: 'A Guy Called Gerald',
+              value: 'A Guy Called Gerald'
+            },
+            {
+              label: 'Kat Richmond',
+              value: 'Kat Richmond'
+            },
+            {
+              label: 'Samantha Blackburn',
+              value: 'Samantha Blackburn'
+            },
+            {
+              label: 'Future Unit',
+              value: 'Future Unit'
+            }
+
+          ]
+
+
+        },
+        {
+
+          venue: 'The Pickle Factory',
+          postcode: ' E2 9DU',
+          skId: '3095944',
+          name: 'The Pickle Factory with Eris Drew & CCL All Night Long',
+          image: 'https://www.residentadvisor.net/images/events/flyer/2019/6/uk-0629-1237853-front.jpg',
+          date: '2019-06-29T23:00:00.000Z',
+          start: '22:00',
+          finish: '04:00',
+          price: 12.50,
+          description: 'Two of the brightest stars in America\'s underground play together all night long at The Pickle Factory. Guided by what she calls The Motherbeat, Eris Drew\'s sets spread love to the dance floor through the speakers. Go to lose yourself dancing, leave spiritually rejuvenated.',
+          minimumAge: 18,
+          modifiedBy: user,
+          createdBy: user,
+          artist: [
+            {
+              label: 'Eris Drew',
+              value: 'Eris Drew'
+            },
+            {
+              label: 'CCL',
+              value: 'CCL'
+            }
+
+
+          ]
+
+
+        },
+        {
+
+          venue: 'Camden Assembly',
+          postcode: ' NW1 8AN',
+          skId: '7786',
+          name: 'Fabio & Grooverider presents 30 Years of Rage',
+          image: 'https://www.residentadvisor.net/images/events/flyer/2019/8/uk-0802-1264769-front.jpg',
+          date: '2019-08-02T23:00:00.000Z',
+          start: '21:30',
+          finish: '03:00',
+          price: 10.00,
+          description: 'Rage is approaching it\'s 30th anniversary. It\'s sonic and cultural legacy is still being felt today, Fabio and Groove are still shutting down raves and festivals every weekend all over the world with their superior DJ sets and musical knowledge guided by their pioneering spirit.',
+          minimumAge: 18,
+          modifiedBy: user,
+          createdBy: user,
+          artist: [
+            {
+              label: 'Fabio',
+              value: 'Fabio'
+            },
+            {
+              label: 'Grooverider',
+              value: 'Grooverider'
+            }
+
+
+          ]
+
+
+        },
+        {
+
+          venue: 'Electrowerkz',
+          postcode: 'EC1V 1NQ;',
+          skId: '3466',
+          name: 'Gaika',
+          image: 'https://www.residentadvisor.net/images/events/flyer/2019/5/uk-0523-1229237-1290617-front.jpg',
+          date: '2019-08-02T23:00:00.000Z',
+          start: '20:00',
+          finish: '23:00',
+          price: 12.50,
+          description: 'GAIKA has never done things in halves. Once part of Manchester rap crew Murkage, the Brixton-born rapper and producer has carved out a unique space for himself in music. 2018’s debut album ‘Basic Volume’ saw him flex his dancehall roots through a turbine of industrial electronics, arresting vocals and hard-hitting lyrics about identity and the bleak landscape of world politics.',
+          minimumAge: 18,
+          modifiedBy: user,
+          createdBy: user,
+          artist: [
+            {
+              label: 'Gaika',
+              value: 'Gaika'
+            }
+
+          ]
+
+
         }
       ])
     })
